0301adf49fd32584c9e3a1a6436885b84681df12
Degoke <69061153+Degoke@users.noreply.github.com>
2020-10-15 18:00:21 +0100

    Basic syntax makjor task

---
+++ BasicSyntaxMajorTask.php
@@ -0,0 +1,27 @@
+<?php
+    //------First Major Task.------
+
+
+    /*
+    Occupation: Student
+    Gender: Male
+    SideHustle Username: DegokeAdewoye
+    */
+
+
+    //---About me tag---
+    echo "<h1>About Me</h1> <hr/>";
+
+    
+    //---My sidehustle username---
+    echo "<h3>DegokeAdewoye</h3>";
+
+    
+    //---Well a little something about me---
+    echo "<p>A lazy Nigerian youth who wants SARS to end. <br/>
+            All i want to do is make money, code, eat pizza 
+            and drink smirnoff ice all day. <br/>
+            I work with python and javascript now its time to learn PHP.
+        </p>"
+
+?>

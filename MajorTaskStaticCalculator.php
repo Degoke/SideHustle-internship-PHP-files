357cf3892abec2ddc49271b52393168d8492763e
Degoke <69061153+Degoke@users.noreply.github.com>
2020-10-16 20:05:41 +0100

    PHP static clculator

---
+++ MajorTaskStaticCalculator.php
@@ -0,0 +1,43 @@
+<?php
+
+    //----I Hereby declare thee variables----
+    $num1 = 23;
+    $num2 = 10;
+
+    
+    //Addition
+    echo "<h1>Addition</h1>";
+    echo "<p>This program adds two variables together</p>";
+    $add = $num1 + $num2;
+    echo "$num1 + $num2 = $add";
+
+
+    //subtraction
+    echo "<h1>Subtraction</h1>";
+    echo "<p>This program subtracts two variables from each other</p>";
+    $subtract = $num1 - $num2;
+    echo "$num1 - $num2 = $subtract";
+
+
+    //multiplication
+    echo "<h1>Multiplication</h1>";
+    echo "<p>This program multiplies two variables together</p>";
+    $multiply = $num1 * $num2;
+    echo "$num1 * $num2 = $multiply";
+
+
+    //division
+    echo "<h1>Division</h1>";
+    echo "<p>This program divides two variables</p>";
+    $divide = $num1 / $num2;
+    echo "$num1 / $num2 = $divide";
+
+
+    //Modulus
+    echo "<h1>Modulus</h1>";
+    echo "<p>This program divides two variables and returns their remainder if there is any.</p>";
+    $modulus = $num1 % $num2;
+    echo "$num1 % $num2 = $modulus";
+
+
+?>
